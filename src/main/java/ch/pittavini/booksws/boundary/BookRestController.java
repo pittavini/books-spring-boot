3308f027f2af497262a0ee4a52614d66ac7dd4a7
Giuseppe Pittavini <giuseppe.pittavini@id.unibe.ch>
2018-07-25 18:23:54 +0200

    Removed order since it's not particularly needed at this point, added find all with limited number of 20 items and a sort field

--- src/main/java/ch/pittavini/booksws/boundary/BookRestController.java
+++ src/main/java/ch/pittavini/booksws/boundary/BookRestController.java
@@ -5,6 +5,8 @@ import javax.validation.Valid;
 
 import ch.pittavini.booksws.control.BookRepository;
 import ch.pittavini.booksws.entity.Book;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -53,5 +56,18 @@ public class BookRestController {
     @ResponseStatus(HttpStatus.ACCEPTED)
     public List find(@PathVariable String title) {
         return bookRepository.findAllByTitleContains(title);
+    }
+
+    /**
+     * Returns a list of books based on sorting field, direction is set to Desc
+     *
+     * @param sortField the sort field
+     * @return a list of books when successful othewise bad request
+     */
+    @GetMapping()
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public List findAll(@RequestParam("sort_field") String sortField) {
+        final PageRequest page = PageRequest.of(0, 20, Sort.Direction.DESC, sortField);
+        return bookRepository.findAll(page).getContent();
     }
 }
