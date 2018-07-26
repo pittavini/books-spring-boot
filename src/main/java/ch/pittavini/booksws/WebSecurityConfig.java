05a8a4135917173ad70c68032ba82db90c10ea09
Giuseppe Pittavini <giuseppe.pittavini@id.unibe.ch>
2018-07-26 10:27:04 +0200

    Fixed delete only for admin users, also removed activemq dependency since it is not needed here

--- src/main/java/ch/pittavini/booksws/WebSecurityConfig.java
+++ src/main/java/ch/pittavini/booksws/WebSecurityConfig.java
@@ -1,23 +1,31 @@
 package ch.pittavini.booksws;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.annotation.EnableJms;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableJms
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.
+                inMemoryAuthentication()
+                .withUser("user").password("{noop}user").roles("USER").and()
+                .withUser("admin").password("{noop}admin").roles("USER","ADMIN");
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/actuator/health").permitAll()
-                .antMatchers("/api/**").hasRole("API")
-                .anyRequest().authenticated()
-                .and().httpBasic()
-                .and().formLogin();
+        http.csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/actuator/health").permitAll()
+            .antMatchers("/api/**").hasAnyRole("ADMIN, USER")
+            .anyRequest().authenticated()
+            .and().httpBasic()
+            .and().formLogin();
     }
 }
